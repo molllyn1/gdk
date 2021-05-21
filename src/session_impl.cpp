0972168c2b79411b4596fb756c2cbb089392bf59
Jon Griffiths <jon_p_griffiths@yahoo.com>
2021-05-21 16:41:45 +1200

    Store and initialize net_params in session_impl
    This standardises the way net_params are used between session types and
    provides a single canonical source of the parameters with user overrides
    applied.
    
    Remove members redundantly and/or inconsistently storing net information by
    always deferring to the net_params directly. Notably, remove accessors
    that can return different data based on potentially inconsistent external
    copies of data that is in net_params itself, i.e. get_connection_string()/
    get_registry_connection_string().
    
    Finally, clean up session_impl includes and get rid of get_json_mut().

--- src/session_impl.cpp
+++ src/session_impl.cpp
@@ -5,7 +5,26 @@
 namespace ga {
 namespace sdk {
 
-    session_impl::session_impl() {}
+    namespace {
+        static network_parameters get_network_overrides(const nlohmann::json& user_params)
+        {
+            // Get the registered network parameters the passed in parameters are based on
+            auto ret = network_parameters::get(user_params.at("name"));
+            // Set override-able settings from the users parameters
+            ret["use_tor"] = user_params.value("use_tor", ret.value("use_tor", false));
+            ret["user_agent"] = user_params.value("user_agent", ret.value("user_agent", std::string()));
+            ret["electrum_url"] = user_params.value("electrum_url", ret.at("electrum_url"));
+            ret["spv_enabled"] = user_params.value("spv_enabled", ret.at("spv_enabled"));
+            ret["tls"] = user_params.value("tls", ret.at("tls"));
+
+            return network_parameters{ ret };
+        }
+    } // namespace
+
+    session_impl::session_impl(const nlohmann::json& net_params)
+        : m_net_params(get_network_overrides(net_params))
+    {
+    }
 
     session_impl::~session_impl() {}
 
